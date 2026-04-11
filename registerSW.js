f6df6ff0176b204eca4065ba1c713f43d5b652a5
bun-97 <bun-97@users.noreply.github.com>
2026-04-11 17:44:44 +0900

    Fix: base path を修正して再デプロイ

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lifeplan-app/sw.js', { scope: '/lifeplan-app/' })})}
